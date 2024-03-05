72d94434fcdeb43ef11390639934a3f341ba9f63
omarouafi <omarouafi12@gmail.com>
2024-03-05 12:22:30 +0100

    ejs setup

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'))
+app.use(require('./routes'));
+
+app.set('view engine', 'ejs');
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
 );
 
+
+app.get('/', (req, res) => {
+    res.render('templates/login');
+});
 
 
