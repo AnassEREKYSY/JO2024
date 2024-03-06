54f6e9093c5771a45c6cf953968724472f4347be
omarouafi <omarouafi12@gmail.com>
2024-03-06 09:53:53 +0100

    public routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const authRoutes = require('./admin/auth.routes');
 const { verify_token } = require('../utils/auth_middlware');
 
-router.use('/admin/sports', verify_token, require('./admin/sports.routes'));
-router.use('/admin/epreuves', verify_token, require('./admin/epreuves.routes'));
-router.use('/admin/athletes', verify_token, require('./admin/athletes.routes'));
-router.use('/admin/resultats', verify_token, require('./admin/resultats.routes'));
-router.use('/admin/titres', verify_token, require('./admin/titres.routes'));
-router.use('/admin/pays', verify_token, require('./admin/pays.routes'));
+// Admin routes
+router.use('/api/admin/sports', verify_token, require('./admin/sports.routes'));
+router.use('/api/admin/epreuves', verify_token, require('./admin/epreuves.routes'));
+router.use('/api/admin/athletes', verify_token, require('./admin/athletes.routes'));
+router.use('/api/admin/resultats', verify_token, require('./admin/resultats.routes'));
+router.use('/api/admin/titres', verify_token, require('./admin/titres.routes'));
+router.use('/api/admin/pays', verify_token, require('./admin/pays.routes'));
+router.use('/api/admin/auth', authRoutes);
 
-router.use('/admin/auth', authRoutes);
+// Public routes
+router.use('/', require('./public/public.routes'));
+
 
 module.exports = router;
